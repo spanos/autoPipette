b0ae1206e996f8dfb936e75dd7d0f5176f9d1493
dspan <david.spanos404@gmail.com>
2014-03-31 21:47:42 -0400

    Incorporated separate motor drive functions into one switch structure.

--- motorTest.c
+++ motorTest.c
@@ -1,7 +1,7 @@
 /* Name: motorTest.c
    Purpose: Simple program to test setup with 
    Author: David Spanos
-   Revision: 0.1
+   Revision: 0.2
      
    Circuit configuration:
    Internal Oscillator with I/O on PGM and PGD, osc speed = 4.0 MHz, 1 us instruction cycle
@@ -16,7 +16,7 @@
                  |                         |
               4- | /MCLR,RA3       RA2,AN2 |-17
                  |                         |
-green LED <<< 5- | RC5, CCP1           RC0 |-16
+              5- | RC5, CCP1           RC0 |-16
                  |                         |
               6- | RC4                 RC1 |-15
                  |                         |
@@ -30,7 +30,6 @@ green LED <<< 5- | RC5, CCP1           RC0 |-16
                  |_________________________|
 */
   
-  
 #device PIC16F1829 ADC=8  
 #include <16F1829-h.h>         // Use my own version of the 1829 header file
 #include <STDLIB.H>            // look in the PIC C User Manual to see what this library contains
@@ -42,39 +41,76 @@ green LED <<< 5- | RC5, CCP1           RC0 |-16
                                // Disable the /MCLR pin so that it can be used as general I/O
                                // Enable the Power-Up Timer, allows power supply to stabilize at start-up        
 #define PIN_B4
-#define motor2_dir PIN_B5
-#define motor2_step PIN_B6
-#define motor1_step PIN_B7
+#define PIN_B5
+#define PIN_B6
+#define motor3_step PIN_B7
 
-#define motor3_dir PIN_C0
-#define motor3_step PIN_C1
+#define PIN_C0
+#define PIN_C1
 #define PIN_C2
-#define PIN_C3
-#define PIN_C4
-#define PIN_C5
-#define PIN_C6             
-#define motor1_dir PIN_C7
+#define motor2_dir PIN_C3
+#define motor1_step PIN_C4
+#define motor1_dir PIN_C5
+#define motor2_step PIN_C6             
+#define motor3_dir PIN_C7
   
-void motor2_fwd() {           //makes one revolution of motor 1                           
-  int i;
-  for (i=0; i<7; i++){
-    delay_ms(10);
-    Output_high(motor2_dir);
-    Output_high(motor2_step);   
+//makes one revolution of motor
+void motor_step(int dir, int motor) {
+  for (int i=0; i<7; i++){
+    delay_us(5);
+    switch(motor){
+      case 1:
+        switch(dir){
+          case 0:
+            Output_high(motor1_dir);
+            break;
+          case 1:
+            Output_low(motor1_dir);
+            break;
+        }
+        Output_high(motor1_step);
+        break;
+      case 2:
+        switch(dir){
+          case 0:
+            Output_high(motor2_dir);
+            break;
+          case 1:
+            Output_low(motor2_dir);
+            break;
+        }
+        Output_high(motor2_step);
+        break;
+      case 3:
+        switch(dir){
+          case 0:
+            Output_high(motor3_dir);
+            break;
+          case 1:
+            Output_low(motor3_dir);
+            break;
+        }
+        Output_high(motor3_step);
+        break;
+    }  
   }                                                        
 }
 
-void motor2_rev() {           //makes one revolution of motor 1                           
-  int i;
-  for (i=0; i<7; i++){
-    delay_ms(10);
-    Output_low(motor2_dir);
-    Output_high(motor2_step);   
-  }                                                        
-}
+//TODO: function that abstracts number of revolutions
+//to distance traveled by component along threaded rod
 
+//Not final values.
 void Main() {
-  While(1) {
-    motor2_fwd();
+  for (int i = 0; i < 10; i++)
+  {
+    motor_step(1,0);
+  }
+  for (int i = 0; i < 10; i++)
+  {
+    motor_step(2,0);
+  }
+  for (int i = 0; i < 10; i++)
+  {
+    motor_step(3,0);
   }
 }
